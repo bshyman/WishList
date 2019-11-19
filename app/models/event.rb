4b4a25e12d184fb1a3cd4fa9ab2dd07f9bde24a6
bshy <bshyman@gmail.com>
2019-11-18 23:03:14 -0600

    add status to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   has_many :user_events, class_name: 'UserEvent'
   has_many :users, through: :user_events
+  
+  enum status: {
+      draft:  0,
+      active: 1
+  }
 end

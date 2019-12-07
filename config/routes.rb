00627e1a917c1871dbf450855f777ffd5e114815
bshy <bshyman@gmail.com>
2019-12-07 16:09:17 -0600

    add some new routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,21 +10,27 @@ Rails.application.routes.draw do
   get 'home/dashboard'
   
   get 'auth/:provider/callback', to: 'sessions#google_oauth', as: 'google_oauth'
-  get 'auth/failure', to: redirect('/')
-  
+  get '/add_gift' => 'gifts#new', as: 'new_gift'
+  get '/new_event' => 'events#new', as: 'new_event'
+  get '/set_selected_event' => 'events#set_selected_event', as:'set_selected_event'
+  get '/join_by_code' => 'events#join_by_code', as: 'join_by_code'
   resources :users do
     collection do
-      get 'notifications'
+      get 'select_event'
       get 'support'
     end
     
     member do
+      get 'my_events'
+      get 'notifications'
       get 'billing'
       get 'profile'
       get 'dashboard'
     end
   end
-  resources :events
-  root 'home#landing'
+  resources :user_events do
+    get 'gifts_for_user'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#landing'
 end

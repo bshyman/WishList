ece44b9bbda25e463aa321fd3587c53bb4ddb0e7
bshy <bshyman@gmail.com>
2020-02-03 20:49:59 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 me = User.find_or_create_by!(email: 'bshyman@gmail.com', first_name: 'b', last_name: 'shy')
-Permission.create!(name: 'superuser')
-Permission.create!(name: 'admin')
-Permission.create!(name: 'basic')
+superuser = Permission.find_by(name: 'superuser')
+admin = Permission.find_by(name: 'admin')
+basic = Permission.find_by(name: 'basic')
 5.times do
-  event = Event.create!(name: Faker::DcComics.title)
-  user  = User.create!(email: Faker::Internet.email, name: Faker::Name.name)
-  UserEvent.create!(user_id: user.id, event_id: event.id, permission_id: 2)
-  UserEvent.create!(user_id: me.id, event_id: event.id, permission_id: 2)
+  event = Event.create!(name: Faker::DcComics.title, join_code: 'secretcode')
+  # user  = User.create!(email: Faker::Internet.email, name: Faker::Name.name)
+  # UserEvent.create!(user_id: user.id, event_id: event.id, permission_id: 2)
+  # UserEvent.create!(user_id: me.id, event_id: event.id, permission_id: 2)
   10.times do
     user = User.create!(email: Faker::Internet.email, name: 'basic user', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-    ue   = UserEvent.create!(user_id: user.id, event_id: event.id, permission_id: 3)
+    ue = UserEvent.create!(user_id: user.id, event_id: event.id, permission_id: 3)
     100.times do
-      Gift.create!(giftee_id: ue.id, gifter_id: UserEvent.where.not(user_id: user.id).sample.id, name: Faker::Ancient.god,
-                  price:         rand(1000), url: Faker::Internet.url)
+      Gift.create!(giftee_id: ue.id, gifter_id: nil, event_id: event.id, name: Faker::Ancient.god, price: rand(1000), url: Faker::Internet.url)
     end
   end
 end
+
+Gift.all.sample(300).each {|gift| gift.update(gifter_id: UserEvent.all.pluck(:id).sample)}
